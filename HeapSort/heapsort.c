673b922e3f4d5c166304bbc8073192d4677a5b3a
Robby Pambudi <robby.pambudi10@gmail.com>
2022-04-09 01:07:15 +0700

    Perbaikan pada HeapSort

--- HeapSort/heapsort.c
+++ HeapSort/heapsort.c
@@ -1,156 +1,191 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#define MaxHeapSize 100000
-void siftUp(int[], int);
+// Heap Sort in C
 
-void heapSort(FILE *in, int sat)
+#include <stdio.h>
+#include <time.h>
+
+// Function to swap the the position of two elements
+void swap(int *a, int *b)
 {
-    int num[MaxHeapSize + 1];
-    int n = 0, number;
-    while (fscanf(in, "%d", &number) == 1)
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(int arr[], int n, int i)
+{
+    // Find largest among root, left child and right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // Swap and continue heapifying if root is not largest
+    if (largest != i)
     {
-        if (n < MaxHeapSize)
-        { // check if array has room
-            num[++n] = number;
-            siftUp(num, n);
-        }
-        else
-        {
-            printf("\nArray too small\n");
-            exit(1);
-        }
-    }
-    if (sat == 1)
-    {
-        FILE *out = fopen("sorted10.txt", "w");
-        for (int i = 1; i <= n; i++)
-        {
-            fprintf(out, "%d\n", num[i]);
-        }
-        fclose(out);
-    }
-    else if (sat == 2)
-    {
-        FILE *out = fopen("sorted100.txt", "w");
-        for (int i = 1; i <= n; i++)
-        {
-            fprintf(out, "%d\n", num[i]);
-        }
-        fclose(out);
-    }
-    else if (sat == 3)
-    {
-        FILE *out = fopen("sorted1000.txt", "w");
-        for (int i = 1; i <= n; i++)
-        {
-            fprintf(out, "%d\n", num[i]);
-        }
-        fclose(out);
-    }
-    else if (sat == 4)
-    {
-        FILE *out = fopen("sorted10000.txt", "w");
-        for (int i = 1; i <= n; i++)
-        {
-            fprintf(out, "%d\n", num[i]);
-        }
-        fclose(out);
-    }
-    else if (sat == 5)
-    {
-        FILE *out = fopen("sorted100000.txt", "w");
-        for (int i = 1; i <= n; i++)
-        {
-            fprintf(out, "%d\n", num[i]);
-        }
-        fclose(out);
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
     }
 }
 
+// Main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(&arr[0], &arr[i]);
+
+        // Heapify root element to get highest element at root again
+        heapify(arr, i, 0);
+    }
+}
+
+// A utility function to print an array of size n
+void printArray(int arr[], int n, int sat)
+{
+    FILE *output;
+    if (sat == 1)
+    {
+        output = fopen("output_10.txt", "w");
+        for (int i = 0; i < n; i++)
+        {
+            fprintf(output, "%d\n", arr[i]);
+        }
+    }
+    else if (sat == 2)
+    {
+        output = fopen("output_100.txt", "w");
+        for (int i = 0; i < n; i++)
+        {
+            fprintf(output, "%d\n", arr[i]);
+        }
+    }
+    else if (sat == 3)
+    {
+        output = fopen("output_1000.txt", "w");
+        for (int i = 0; i < n; i++)
+        {
+            fprintf(output, "%d\n", arr[i]);
+        }
+    }
+    else if (sat == 4)
+    {
+        output = fopen("output_10000.txt", "w");
+        for (int i = 0; i < n; i++)
+        {
+            fprintf(output, "%d\n", arr[i]);
+        }
+    }
+    else if (sat == 5)
+    {
+        output = fopen("output_100000.txt", "w");
+        for (int i = 0; i < n; i++)
+        {
+            fprintf(output, "%d\n", arr[i]);
+        }
+    }
+    fclose(output);
+}
+void scaningNumber(FILE *fptr, int *arr, int *n)
+{
+
+    int i = 0;
+    while (fscanf(fptr, "%d", &arr[i]) == 1)
+    {
+        i++;
+        *n += 1;
+    }
+}
+// Driver code
 int main()
 {
-    // Setting Time start
-    FILE *out;
-    FILE *fp;
-    out = fopen("output.txt", "w");
-    clock_t start = clock();
-    // Untuk Input 10 Number
-    fp = fopen("input10.txt", "r");
-    heapSort(fp, 1);
-    fclose(fp);
-    // Time end
-    clock_t end = clock();
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    fprintf(out, "1.\t10 data diporeleh waktu \t:");
-    fprintf(out, "\t %f ms\n", time_spent);
+    int arr[100000];
+    int n = 0;
+    FILE *in;
+    FILE *kesimpulan;
+    // Seting time variable
+    clock_t starttime, endtime;
 
-    // Setting Time start
-    start = clock();
-    // Untuk Input 100 Number
-    fp = fopen("input100.txt", "r");
-    heapSort(fp, 2);
-    fclose(fp);
-    // Time end
-    end = clock();
-    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    fprintf(out, "2.\t100 data diporeleh waktu \t:");
-    fprintf(out, "\t %f ms\n", time_spent);
+    // 10 Data
+    in = fopen("input10.txt", "r");
+    scaningNumber(in, arr, &n);
+    starttime = clock();
+    // Sorted
+    heapSort(arr, n);
+    endtime = clock();
+    // Print sorted array
+    printArray(arr, n, 1);
+    // Print time
+    kesimpulan = fopen("kesimpulan.txt", "a");
+    fprintf(kesimpulan, "10 Data memakan waktu : %f\n", (double)(endtime - starttime) / CLOCKS_PER_SEC);
+    fclose(in);
 
-    // Setting Time start
-    start = clock();
-    // Untuk Input 1000 Number
-    fp = fopen("input1000.txt", "r");
-    heapSort(fp, 3);
-    fclose(fp);
-    // Time end
-    end = clock();
-    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    fprintf(out, "3.\t1000 data diporeleh waktu \t:");
-    fprintf(out, "\t %f ms\n", time_spent);
+    // 100 Data
+    n = 0;
+    in = fopen("input100.txt", "r");
+    scaningNumber(in, arr, &n);
+    starttime = clock();
+    // Sorted
+    heapSort(arr, n);
+    endtime = clock();
+    // Print sorted array
+    printArray(arr, n, 2);
+    // Print time
+    fprintf(kesimpulan, "100 Data memakan waktu : %f\n", (double)(endtime - starttime) / CLOCKS_PER_SEC);
+    fclose(in);
 
-    // Setting Time start
-    start = clock();
-    // Untuk Input 10000 Number
-    fp = fopen("input10000.txt", "r");
-    heapSort(fp, 4);
-    fclose(fp);
-    // Time end
-    end = clock();
-    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    fprintf(out, "4.\t10000 data diporeleh waktu \t:");
-    fprintf(out, "\t %f ms\n", time_spent);
+    // 1000 data
+    n = 0;
+    in = fopen("input1000.txt", "r");
+    scaningNumber(in, arr, &n);
+    starttime = clock();
+    // Sorted
+    heapSort(arr, n);
+    endtime = clock();
+    // Print sorted array
+    printArray(arr, n, 3);
+    // Print time
+    fprintf(kesimpulan, "1000 Data memakan waktu : %f\n", (double)(endtime - starttime) / CLOCKS_PER_SEC);
+    fclose(in);
 
-    // Setting Time start
-    start = clock();
-    // Untuk Input 100000 Number
-    fp = fopen("input100000.txt", "r");
-    heapSort(fp, 5);
-    fclose(fp);
-    // Time end
-    end = clock();
-    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    fprintf(out, "5.\t100000 data diporeleh waktu :");
-    fprintf(out, "\t %f ms\n", time_spent);
-    fclose(out);
+    // 10000 data
+    n = 0;
+    in = fopen("input10000.txt", "r");
+    scaningNumber(in, arr, &n);
+    starttime = clock();
+    // Sorted
+    heapSort(arr, n);
+    endtime = clock();
+    // Print sorted array
+    printArray(arr, n, 4);
+    // Print time
+    fprintf(kesimpulan, "10000 Data memakan waktu : %f\n", (double)(endtime - starttime) / CLOCKS_PER_SEC);
+    fclose(in);
 
-    return 0;
+    // 100000 data
+    n = 0;
+    in = fopen("input100000.txt", "r");
+    scaningNumber(in, arr, &n);
+    starttime = clock();
+    // Sorted
+    heapSort(arr, n);
+    endtime = clock();
+    // Print sorted array
+    printArray(arr, n, 5);
+    // Print time
+    fprintf(kesimpulan, "100000 Data memakan waktu : %f\n", (double)(endtime - starttime) / CLOCKS_PER_SEC);
+    fclose(in);
 
-} // end main
+    fclose(kesimpulan);
 
-void siftUp(int heap[], int n)
-{
-    // sifts up the value in heap[n] so that heap[1..n] contains a heap
-    int siftItem = heap[n];
-    int child = n;
-    int parent = child / 2;
-    while (parent > 0)
-    {
-        if (siftItem <= heap[parent])
-            break;
-        heap[child] = heap[parent]; // move down parent
-        child = parent;
-        parent = child / 2;
-    }
-    heap[child] = siftItem;
-} // end siftUp
+    printf("Array is Sorted \n");
+}
