03be5017a8a091bdd8e3a3f23a6ff00a856c4fc4
Robby Pambudi <robby.pambudi10@gmail.com>
2022-04-11 11:53:41 +0700

    Soal No 9

---
+++ Soal No 9/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Write a recursive function for finding the kth smallest number in an array of n numbers, without sorting the array.
+void findKthSmallest(int arr[], int n, int *min)
+{
+    if (n == 0)
+    {
+        *min = arr[0];
+    }
+    else
+    {
+        findKthSmallest(arr, n -= 1, min);
+        if (arr[n] < *min)
+        {
+            *min = arr[n];
+        }
+    }
+}
+void scanNumber(int arr[], int *a)
+{
+    for (int i = 0; i < *a; i++)
+    {
+        printf("Masukkan angka ke-%d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+}
+
+int main()
+{
+    int arr[1000];
+    int n;
+    int min = 10000;
+    printf("Masukkan jumlah elemen : ");
+    scanf("%d", &n);
+    scanNumber(arr, &n);
+    findKthSmallest(arr, n, &min);
+    printf("Nilai Minimum dari array adalah %d\n", min);
+    printf("\n");
+    return 0;
+}
