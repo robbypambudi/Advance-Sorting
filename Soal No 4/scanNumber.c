2b855195f3ad726043c243c79659ae627b704ea9
Robby Pambudi <robby.pambudi10@gmail.com>
2022-04-11 09:51:07 +0700

    Tambahan deskripsu

--- Soal No 4/scanNumber.c
+++ Soal No 4/scanNumber.c
@@ -14,14 +14,29 @@ Fungsi Anda akan berfungsi jika heap awalnya kosong dan akan mencetak pesan jika
 untuk menyimpan v.
 */
 
-void scanNumber(int arr[])
+/*
+Penyelesaain :
+1. Membuat fungsi untuk menambahkan nilai v ke heap.
+2. Membuat fungsi untuk mencetak pesan jika tidak ada ruang untuk menyimpan v.
+
+*/
+
+void scanNumber(int arr[], int *a)
 {
     int i, n;
     printf("Masukkan jumlah elemen : ");
     scanf("%d", &n);
-    for (i = 0; i < n; i++)
-    {
-        printf("Masukkan elemen ke-%d : ", i + 1);
-        scanf("%d", &arr[i]);
+    if (n == 0)
+    { // Fungsi untuk mencetak pesan jika tidak ada ruang untuk menyimpan v.
+        puts("Tidak ada elemen");
     }
+    else
+    {
+        for (i = 0; i < n; i++)
+        { // Fungsi untuk menambahkan nilai v ke heap.
+            printf("Masukkan elemen ke-%d : ", i + 1);
+            scanf("%d", &arr[i]);
+        }
+    }
+    *a = n;
 }
