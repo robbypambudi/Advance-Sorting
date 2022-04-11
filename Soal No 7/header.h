3507ec754cf69ec60d9dbb1d78b024752cd52207
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2022-04-11 11:10:36 +0700

    update No 7

---
+++ Soal No 7/header.h
@@ -0,0 +1,21 @@
+/*
+Nama Kelompok :
+1. Robby Ulung Pambudi
+2. M. Armand Giovari
+3. Fathin Muhashibi Putra
+*/
+
+/*
+Deskripsi :
+Tulis sebuah fungsi yang, jika diberikan bilangan bulat array dan bilangan bulat n, mengurutkan elemen num[1]
+ke num[n] menggunakan Shell sort. Fungsi harus mengembalikan jumlah perbandingan kunci yang dibuat di
+melakukan pengurutan. Anda dapat menggunakan metode apa pun yang masuk akal untuk menentukan kenaikan.
+
+Penyelesaian :
+
+*/
+
+#include <stdio.h>
+
+void shellSort(int array[], int n);
+void printArray(int array[], int size);
--- Soal No. 3/heapsort.c
+++ Soal No. 3/heapsort.c
@@ -1,15 +1,17 @@
 /*
-Kelompok 9 :
-- Robby Ulung Pambudi 
-- Fathin Muhashibi Putra
-- M. Armand Giovani
+Nama Kelompok :
+1. Robby Ulung Pambudi
+2. M. Armand Giovari
+3. Fathin Muhashibi Putra
+*/
 
-DESKRIPSI :
+/*
+Deskripsi :
 Heap disimpan dalam array integer satu dimensi num[1..n] dengan nilai terbesar di posisi 1. 
 Berikan algoritma efisien yang menghapus root dan mengatur ulang elemen lainnya
 sehingga heap sekarang menempati num[1] hingga num[n-1].
 
-PENYELESAIAN :
+Penyelesaian :
 
 */
 
