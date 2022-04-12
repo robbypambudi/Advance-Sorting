0c4a60f2ac0e4760ec7be27633cdc05e6a8a8c84
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2022-04-12 15:03:57 +0700

    tambah deskripsi

--- Soal No 5/header.h
+++ Soal No 5/header.h
@@ -1,8 +1,23 @@
-// Deskripsi
+/*
+Nama Kelompok :
+1. Robby Ulung Pambudi
+2. M. Armand Giovari
+3. Fathin Muhashibi Putra
+*/
 
-// Penyelesaian:
+/*
+Deskripsi :
+Tulis kode untuk membaca satu set bilangan bulat positif (diakhiri oleh 0) dan buat tumpukan dalam array H
+dengan nilai terkecil di bagian atas tumpukan. Saat setiap bilangan bulat dibaca, itu dimasukkan di antara
+item yang ada sehingga properti heap dipertahankan. Setiap saat, jika n angka telah dibaca, maka H[1..n] 
+harus berisi heap. Asumsikan bahwa H cukup besar untuk menampung semua bilangan bulat.
 
-// Heap Sort in C
+Diketahui data 51 26 32 45 38 89 29 58 34 23 0, tunjukkan isi H setelah masing-masing
+nomor telah dibaca dan diproses.
+
+Penyelesaian :
+
+*/
 
 #include <stdio.h>
 
