4fb468c9c8bc1aa7a0bd25acefc1435caef6e022
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2022-04-12 21:31:16 +0700

    Update deskripsi

--- Soal No 4/header.h
+++ Soal No 4/header.h
@@ -1,3 +1,26 @@
+/*
+Nama Kelompok :
+1. Robby Ulung Pambudi
+2. M. Armand Giovari
+3. Fathin Muhashibi Putra
+*/
+
+/*
+Deskripsi :
+
+heap disimpan dalam array integer satu dimensi A[0..max] dengan nilai terbesar di posisi 1. A[0]
+menentukan jumlah elemen di heap setiap saat. Tulis fungsi untuk menambahkan nilai baru v ke heap.
+Fungsi Anda akan berfungsi jika heap awalnya kosong dan akan mencetak pesan jika tidak ada ruang
+untuk menyimpan v.
+*/
+
+/*
+Penyelesaain :
+1. Membuat fungsi untuk menambahkan nilai v ke heap.
+2. Membuat fungsi untuk mencetak pesan jika tidak ada ruang untuk menyimpan v.
+
+*/
+
 #include <stdio.h>
 
 void scanNumber(int arr[], int *a);
